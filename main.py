c4f350bbf804296a104ed6adea9bad5f15850186
Pablo <pablo.compte.jeux@gmail.com>
2023-12-21 11:30:24 +0100

    final version

--- main.py
+++ main.py
@@ -23,18 +23,17 @@ if __name__ == "__main__":
     print("You can choose between the following options:")
     print("1. Display the names of the studied presidents")
     print("2. Display all the words used in speeches and their count")
-    print("3. Display the idf score of the words used by presidents")
-    print("4. Display the least important word(s) in all speeches")
-    print("5. Display the most important word(s) in all speeches")
-    print("6. Display the most used word in all speeches")
-    print("7. Display the most used word in a specific speech")
-    print("8. Display the speeches that contain a specific word")
-    print("9. Display the first speeches to talk about a specific topic")
-    print("10. Count how much a word is said in all speeches")
-    print("11. Ask antoher question")
-    print("12. Exit the program")
+    print("3. Display the least important word(s) in all speeches")
+    print("4. Display the most important word(s) in all speeches")
+    print("5. Display the most used word in all speeches")
+    print("6. Display the most used word in a specific speech")
+    print("7. Display the presidents that told a specific word")
+    print("8. Display the first speeches to talk about a specific topic")
+    print("9. Count how much a word is said in all speeches")
+    print("10. Ask antoher question")
+    print("11. Exit the program")
     choice = input("Please enter the number of the option you want to choose: ")
-    while choice != "12":
+    while choice != "11":
         if choice == "1":
             print("Sure ! Here are the names of the presidents:")
             temp = names(pres_names(directory + "/speeches"))
@@ -46,27 +45,23 @@ if __name__ == "__main__":
             for i, j in e.items():
                 print(i, ":", j)
         elif choice == "3":
-            print("Sure ! Here are the idf of the words used by presidents:")
-            for i, j in countIdf.items():
-                print(i, ":", j)
-        elif choice == "4":
             print("Sure ! Here are the least important words in all speeches:")
             lowest = lowest_td_idf(directory, countIdf)
             for i in lowest:
                 print(i)
             print("These words are not important, meaning that their tf-idf "
                   "score is 0!")
-        elif choice == "5":
+        elif choice == "4":
             print("Sure ! Here are the most important words in all speeches:")
             highest_td_idf(directory, countIdf)
             print("These words are important because they are used in only one speech !")
-        elif choice == "6":
+        elif choice == "5":
             print("Sure ! Here is the most used word in all speeches:")
             temp = max(count_words_total(directory).values())
             for i, j in count_words_total(directory).items():
                 if temp == j:
                     print(i, ":", j)
-        elif choice == "7":
+        elif choice == "6":
             print("Sure ! Here is the most used word in a specific speech:")
             print("Here are the names of the speeches:")
             for filename in listdir(directory + "\clean"):
@@ -81,11 +76,10 @@ if __name__ == "__main__":
             for i, j in count_words(filename, directory).items():
                 if temp == j:
                     print(i, ":", j)
-        elif choice == "8":
+        elif choice == "7":
             print("Sure ! Here are the presidents who told a specific word:")
             word = input("Please enter the word you want to search: ")
             word = word.lower()
-            print("Here are the president that told the word ", word, ":")
             temp, temp2 = [],[]
             for filename in listdir(directory + "\clean"):
                 if word in count_words(filename, directory):
@@ -96,10 +90,11 @@ if __name__ == "__main__":
                         if k in cell and i not in temp2:
                             temp2.append(i)
             for cell in temp2:
+                print("Here are the president that told the word ", word, ":")
                 print(cell)
             if not temp2:
                 print("Sorry, no president told this word.")
-        elif choice == "9":
+        elif choice == "8":
             print("Sure ! Here is the first speech to talk about a specific topic:")
             word = input("Please enter the word you want to search: ")
             word = word.lower()
@@ -115,10 +110,10 @@ if __name__ == "__main__":
                 print("Sorry, no president talked about this word.")
             else:
                 print(test[0])
-        elif choice == "11":
+        elif choice == "10":
             question = input("Sure ! Enter a question and i'll answer from the speeches !")
             choosefile(directory, question, countIdf)
-        elif choice == "10":
+        elif choice == "9":
             print("Sure ! Here is the number of times a word is said in all speeches:")
             word = input("Please enter the word you want to search: ")
             word = word.lower()
